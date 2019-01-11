02a8ad10f033e3050142380826157ed131947e41
Giselle <giselle.salgado@speeracademy.org>
2019-01-11 13:56:38 -0600

    SI19-25 Removed the search boxes, fixed the footer, and added a redirect feature

--- 404.php
+++ 404.php
@@ -5,33 +5,27 @@
     <div class="row">
       <div class="post-list <?php if (!is_active_sidebar('sidebar-1')) echo 'post-list-large'; ?>">
         <div class="post-list-c">
-        <?php
-            if (have_posts()):
-              while (have_posts()):
-                the_post();
-                get_template_part('template-parts/content', get_post_format());
-              endwhile;
-            else:
-              get_template_part('template-parts/content', 'none');
-            endif;
-        ?>
+        
         </div>
         <div class="navigation-c">
-          <?php  
-              if (have_posts()):
-                one_page_express_pagination(array(
-                    'prev_text'          => __('Older posts', 'one-page-express'),
-                    'next_text'          => __('Newer posts', 'one-page-express'),
-                    'before_page_number' => '<span class="meta-nav screen-reader-text">' . __('Page', 'one-page-express') . ' </span>',
-                ));
-              endif;
-          ?>
+          
+
+
+<h4> <a href="https://nobleforward.org/">Click here to return to home page if you're not redirected</a>
+ </h4>
+	
+	<script>
+        var timer = setTimeout(function() {
+            window.location='https://nobleforward.org/'
+        }, 3000);
+    </script>
+
+
+			
         </div>
       </div>
-      <?php get_sidebar();?>
+    
     </div>
   </div>
 </div>
-
-<?php one_page_express_get_footer(); 
-
+ <?php get_footer(); ?>
