0c6bb7f626d5d783f197d51f9fa4238612a9d52e
Joe Ravesloot <jravesloot@noblenetwork.org>
2019-01-16 14:45:24 -0600

    CS19-25 Tidy up formatting

--- 404.php
+++ 404.php
@@ -4,28 +4,17 @@
   <div class="gridContainer">
     <div class="row">
       <div class="post-list <?php if (!is_active_sidebar('sidebar-1')) echo 'post-list-large'; ?>">
-        <div class="post-list-c">
-        
-        </div>
         <div class="navigation-c">
-          
-
-
-<h4> <a href="https://nobleforward.org/">Click here to return to home page if you're not redirected</a>
- </h4>
-	
-	<script>
-        var timer = setTimeout(function() {
+	  <h4> <a href="https://nobleforward.org/">Click here to return to home page if you're not redirected</a></h4>
+	  <script>
+            var timer = setTimeout(function() {
             window.location='https://nobleforward.org/'
-        }, 3000);
-    </script>
-
-
-			
+            }, 3000);
+          </script>
         </div>
       </div>
-    
     </div>
   </div>
 </div>
- <?php get_footer(); ?>
+
+<?php get_footer(); ?>
